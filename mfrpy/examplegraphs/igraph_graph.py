ebd9806850d9f9738dcedb4d80f19503f2f5bc39
Igor Sokolov <igor.sokolov@ufl.edu>
2023-01-16 21:25:07 -0500

    reorganized package, implemented involution

--- mfrpy/examplegraphs/igraph_graph.py
+++ mfrpy/examplegraphs/igraph_graph.py
@@ -1,22 +1,28 @@
-from igraph import *
+from igraph import Graph
 
 # example graphs from Wang et al. 2013
 dag = Graph(directed = True) # directed acyclic graph, no composite nodes
 dag.add_vertices(10)
 dag.add_edges([
-(0,1), (0,2), (0,3), (1,4), (2,5), (1,5), (3,2), (3,6), (4,7),
-(5,7), (5,9), (7,9), (6,8), (6,9), (8,9)
-])
-dag.vs["name"] = ["i", "a", "b", "c", "d", "e", "f", "g", "h", "o"]
+    (0,1), (0,2), (0,3), (1,4),
+    (2,5), (1,5), (3,2), (3,6),
+    (4,7), (5,7), (5,9), (7,9),
+    (6,8), (6,9), (8,9)
+    ])
+dag.vs["name"] = [
+    "i", "a", "b", "c", "d",
+    "e", "f", "g", "h", "o"
+    ]
 dag.es["synergy"] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 2, 0, 0, 2, 0]
 # edges (1, 5) & (2, 5) and (5, 9) & (6, 9) have synergy
 
 dcg = Graph(directed = True) # directed cyclic graph, no composite nodes
 dcg.add_vertices(8)
 dcg.add_edges([
-(0,1), (0,2), (0,3), (3,4), (4,2), (2,5), (1,5), (5,4), (4,6),
-(6,5), (5,7), (6,7)
-])
+    (0,1), (0,2), (0,3), (3,4),
+    (4,2), (2,5), (1,5), (5,4),
+    (4,6), (6,5), (5,7), (6,7)
+    ])
 dcg.vs["name"] = ["i", "a", "b", "c", "d", "e", "f", "o"]
 dcg.es["synergy"] = [0, 1, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0]
 # edges (1, 5) & (2, 5) and (0, 2) & (4, 2) have synergy
@@ -24,11 +30,16 @@ dcg.es["synergy"] = [0, 1, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0]
 exp_dag = Graph(directed = True) # immitates expanded dag graph
 exp_dag.add_vertices(12)
 exp_dag.add_edges([
-(0,1), (0,2), (0,3), (1,4), (3,2), (3,6), (4,7), (5,7),
-(7,9), (6,8), (8,9), (5,10), (6,10), (10,9), (1,11), (2,11), (11,5)
-])
-exp_dag.vs["name"] = ["i", "a", "b", "c", "d", "e", "f", "g", "h", "o", "c1",
-"c2"]
+    (0,1), (0,2), (0,3), (1,4),
+    (3,2), (3,6), (4,7), (5,7),
+    (7,9), (6,8), (8,9), (5,10),
+    (6,10), (10,9), (1,11), (2,11),
+    (11,5)
+    ])
+exp_dag.vs["name"] = [
+    "i", "a", "b", "c", "d", "e",
+    "f", "g", "h", "o", "c1","c2"
+    ]
 exp_dag.vs["composite"] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
 # exp_dag.composite_nodes = [10, 11]
 # nodes 10 and 11 are composite nodes
@@ -36,10 +47,15 @@ exp_dag.vs["composite"] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
 exp_dcg = Graph(directed = True) # immitates expanded dcg graph
 exp_dcg.add_vertices(10)
 exp_dcg.add_edges([
-(0,1), (0,3), (3,4), (5,4), (4,6),
-(6,5), (5,7), (6,7), (0,8), (4,8), (8,2), (1,9), (2,9), (9,5)
-])
-exp_dcg.vs["name"] = ["i", "a", "b", "c", "d", "e", "f", "o", "c1", "c2"]
+    (0,1), (0,3), (3,4), (5,4),
+    (4,6), (6,5), (5,7), (6,7),
+    (0,8), (4,8), (8,2), (1,9),
+    (2,9), (9,5)
+    ])
+exp_dcg.vs["name"] = [
+    "i", "a", "b", "c", "d",
+    "e", "f", "o", "c1", "c2"
+    ]
 exp_dag.vs["composite"] = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
 # exp_dcg.composite_nodes = [8, 9]
 # nodes 8 and 9 are composite nodes
@@ -47,18 +63,20 @@ exp_dag.vs["composite"] = [0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
 # example graph with a loop
 self_loop = Graph(directed = True)
 self_loop.add_vertices(5)
-self_loop.add_edges(
-[(0,1), (0,2), (1,4), (1,3), (2,3), (3,3), (3,4)]
-)
+self_loop.add_edges([
+    (0,1), (0,2), (1,4), (1,3),
+    (2,3), (3,3), (3,4)
+    ])
 self_loop.vs["name"] = ["i", "a", "b", "c", "o"]
 self_loop.es["synergy"] = [0, 0, 0, 1, 1, 0, 0]
 
 # example graph from Wang & Albert 2011, modified to include synergy
 inhib = Graph(directed = True)
 inhib.add_vertices(6)
-inhib.add_edges(
-[(0,1), (0,2), (0,3), (1,3), (2,3), (3,4), (4,5)]
-)
+inhib.add_edges([
+    (0,1), (0,2), (0,3), (1,3),
+    (2,3), (3,4), (4,5)
+    ])
 inhib.vs["name"] = ["i", "a", "b", "c", "d", "o"]
 inhib.es["synergy"] = [0, 0, 0, 1, 1, 0, 0]
 inhib.es["inhibition"] = [0, 0, 1, 1, 1, 1, 0]
@@ -67,8 +85,10 @@ inhib.es["inhibition"] = [0, 0, 1, 1, 1, 1, 0]
 # example for testing redundancy in sgmfr i.e. nodes that cannot be activated
 impossible = Graph(directed = True)
 impossible.add_vertices(5)
-impossible.add_edges([(0,2), (1,2), (2,4), (3,4)])
-impossible.vs["name"] = ["A", "B", "C", "D", "E"]
+impossible.add_edges([
+    (0,2), (1,2), (2,4), (3,4)
+    ])
+impossible.vs["name"] = ["a", "b", "c", "d", "e"]
 impossible.es["synergy"] = [1, 1, 2, 2]
 impossible.es["inhibition"] = [0, 1, 1, 0]
 
@@ -76,3 +96,13 @@ impossible.es["inhibition"] = [0, 1, 1, 0]
 bordetellae = Graph.Read_GraphML("bordetellaeGraph.xml")
 bordetellae.vs["name"] = bordetellae.vs["id"]
 bordetellae.vs["label"] = bordetellae.vs["name"]
+
+# example graph with multiple edges that are part of distinct synergies
+manysyns = Graph(directed = True)
+manysyns.add_vertices(5)
+manysyns.add_edges([
+    (0,1), (0,2), (0,3), (1,4),
+    (2,4), (2,4), (3,4)
+    ])
+manysyns.vs["name"] = ["a", "b", "c", "d", "e"]
+manysyns.es["synergy"] = [0, 0, 0, 1, 1, 2, 2]
--- mfrpy/sgmfr.py
+++ mfrpy/sgmfr.py
@@ -28,29 +28,33 @@ def get_mfrs(graph, source, target, verbose = False, mode = "es"):
 
     """
 
+    # Extract attributes from graph if they exist
     oggraph = graph
-    synergistic = []
-    if 'synergy' in graph.es.attributes():
+    try:
         synergistic = graph.es["synergy"]
-    negatory = []
-    if 'inhibition' in graph.es.attributes():
-        negatory = graph.es["inhibition"]
-    # expansion of graph to include composite and inhibitory nodes
-    graph = update_expand.expand(graph, update_expand.updates(graph,
-    synergistic, negatory))
+    except KeyError:
+        synergistic = []
 
-    # initialization
+    try:
+        negatory = graph.es["inhibition"]
+    except KeyError:
+        negatory = []
+
+    # Expansion of graph to include composite and inhibitory nodes
+    graph = update_expand.expand(
+        graph,
+        update_expand.updates(graph, synergistic, negatory)
+        )
+    # Initialization of variables for main loop
     pointer = 0
     num = 1
-    # tags is an array of integers that keeps track of the index of a node in
-    # the current MFR
+    # tags is an array that keeps track of node indices in current MFR
     tags = [0]
     flag = False
     discard = []
     net = graph.get_adjlist(mode='in')
     all_MFRs = [[[target, net[target]]]]
-
-    #keeps track of nodes with no predecessors that are not the source
+    # redundant tracks nodes with no predecessors that are not the source
     redundant = []
     v = 0
     while v < len(graph.vs()):
@@ -61,126 +65,142 @@ def get_mfrs(graph, source, target, verbose = False, mode = "es"):
 
     if verbose:
         print("nodes with no predecessors (other than source):", redundant)
-        print("composite list:", graph.vs["composite"])
 
-    # while some partial MFRs remain
+    # Main loop, while some partial MFRs remain unfinished
     while pointer < num:
         flag = False
         c_MFR = all_MFRs[pointer]
         c_tag = tags[pointer]
+
         if verbose:
             print("current MFR:", all_MFRs[pointer])
-        # while the current MFR is incomplete
+
+        # While the current MFR is unfinished
         while not flag:
             c_node = c_MFR[c_tag][0]
-            # c_preds is a list of integers
             c_preds = c_MFR[c_tag][1]
+
             if verbose:
                 print("node and predecessors:", c_node, c_preds)
-            # if no predecessors remain
-            # python uses implicit booleans for lists
+
+            # If no predecessors remain
+            # Note: python uses implicit booleans for lists
             if not c_preds:
+
                 if verbose:
                     print("node", c_node, "has no predecessors")
+
                 if c_tag == len(c_MFR) - 1:
                     flag = True
+
                     if verbose:
                         print("MFR finished")
+
                 else:
                     c_tag = c_tag + 1
 
             else:
-                # if current node is not composite
+                # If current node is not composite
                 if not graph.vs[c_node]["composite"]:
                     m = len(c_preds)
                     c_MFR[c_tag][1] = c_preds[0]
-
-                    # allots memory to new partial MFRs
+                    # Allots memory to new partial MFRs
                     i = 0
                     while i < m - 1:
-                        # creates a copy of the current MFR for each predecessor
+                        # Creates a copy of the current MFR for each predecessor
                         temp1 = [li[:] for li in c_MFR]
                         temp1[c_tag][1] = [c_preds[i + 1]]
-                        # current MFR is replaced by first copy
+                        # Current MFR is replaced by first copy
                         all_MFRs.append(temp1)
                         tags.append(c_tag)
                         i = i + 1
                     num = num + m - 1
                     c_preds = [c_MFR[c_tag][1]]
 
-                # list of entries in MFR's first "column"
+                # List of entries in MFR's first "column" (from nodes)
                 stems = [row[0] for row in c_MFR]
-
-                # checks that all c_preds are in c_MFR
+                # Checks that all c_preds are in c_MFR
                 if not set(c_preds).difference(set(stems)):
+
                     if verbose:
                         print("all predecessors are in current MFR")
+
                     if c_tag == len(c_MFR) - 1:
                         flag = True
+
                         if verbose:
                             print("MFR finished")
+
                     else:
                         c_tag = c_tag + 1
 
-                # appends new rows to current partial MFR
+                # Appends new rows to current partial MFR
                 else:
                     for v in c_preds:
                         temp2 = net[v]
+
                         if verbose:
                             print("preds of preds:", temp2)
+
+                        # If the node cannot be activated we ignore this MFR
                         if set(temp2).intersection(set(redundant)):
                             if graph.vs[v]["composite"]:
+                                flag = True
+
                                 if verbose:
                                     print("node can't be activated")
+
                                 discard.append(c_MFR)
-                                flag = True
                         else:
                             c_MFR.append([v, temp2])
                     c_tag = c_tag + 1
+
                     if verbose:
                         print("new partial MFR:", c_MFR)
 
         pointer = pointer + 1
 
-        # list of entries in MFRs second 'column'
+        # list of entries in MFRs second "column" (to nodes)
         stalks = [row[1] for row in c_MFR]
-        # checks for discard and adds 'extraneous' MFRs to 'discard' list
+        # checks for impossible edges and adds extraneous MFRs to discard
         if not [] in stalks:
             discard.append(c_MFR)
+
         if verbose:
             print("discard:", discard)
 
-    # removes "extra" MFRs, those that result from discard in graph and non-
-    # activated nodes
+    # Removes extra MFRs from cycles in graph or in discard list
     final_MFRs = []
     for mfr in all_MFRs:
         if not mfr in discard:
             final_MFRs.append(mfr)
 
     for mfr in final_MFRs:
-        # removes unecessary "last" row of mfr
+        # Removes unecessary last row of MFR
         for item in mfr:
             if item == [0,[]]:
                 mfr.remove(item)
-        # reverses order of everything (since alg is bottom-up)
+        # Reverses order of lists (since algorithm is bottom-up)
         mfr.reverse()
         for item in mfr:
             item.reverse()
-        # flattens list
+        # Flattens list
         for item in mfr:
             if type(item[0]) is list:
                 for i in item[0]:
                     mfr.insert(mfr.index(item) + 1, [i, item[1]])
                 mfr.remove(item)
 
-    # translates expanded vertices to original
+    # Translates expanded vertices to original
     contracted_MFRs = []
     for mfr in final_MFRs:
         new_mfr = []
+
         if verbose:
             print("old mfr:", mfr)
+
         for edge in mfr:
-            # contracts composite edges by deleting composite nodes
+            # Contracts composite edges by deleting composite nodes
             if graph.vs[edge[0]]["composite"]:
                 for e in mfr:
                     if e[1] == edge[0]:
@@ -189,44 +209,54 @@ def get_mfrs(graph, source, target, verbose = False, mode = "es"):
                 pass
             else:
                 new_mfr.append(edge)
-        # changes inhibitory nodes back to originals
+
+        # Changes inhibitory nodes back to originals
         for edge in new_mfr:
             if '~' in graph.vs[edge[0]]["name"]:
-                dict = {edge[0]:graph.vs["name"].index("{}".format(to_dnf(
-                "~{}".format(graph.vs[edge[0]]["name"]))))}
+                dict = {edge[0]:graph.vs["name"].index(
+                "{}".format(to_dnf("~{}".format(
+                graph.vs[edge[0]]["name"]
+                ))))}
                 edge[0]=dict[edge[0]]
             elif '~' in graph.vs[edge[1]]["name"]:
-                dict = {edge[1]:graph.vs["name"].index("{}".format(to_dnf(
-                "~{}".format(graph.vs[edge[1]]["name"]))))}
+                dict = {edge[1]:graph.vs["name"].index(
+                "{}".format(to_dnf("~{}".format(
+                graph.vs[edge[1]]["name"]
+                ))))}
                 edge[1]=dict[edge[1]]
         contracted_MFRs.append(new_mfr)
+
         if verbose:
             print("new mfr:", new_mfr)
-    if verbose:
-        for mfr in contracted_MFRs:
-            print(mfr)
 
     final_MFRs = contracted_MFRs
-    # output options
-    if verbose:
-        print("Number of MFRs:", len(final_MFRs), "\n")
 
+    if verbose:
+        print("Number of MFRs:", len(final_MFRs))
+
+    # Output options
     if mode == "em": # "em" = edge matrix
         ind = 1
         for mfr in final_MFRs:
+
             if verbose:
                 print("\n", ind, ":")
                 for chunk in mfr:
                     print(chunk)
+
             ind += 1
         return [final_MFRs, len(final_MFRs)]
+
     elif mode == "el": # "el" = edge list
         ind = 1
         for mfr in final_MFRs:
+
             if verbose:
                 print("\n", ind, ":", mfr)
+
             ind += 1
         return [final_MFRs, len(final_MFRs)]
+
     elif mode == "es": # "es" = edge sequence ids
         ids = []
         ind = 1
@@ -236,8 +266,10 @@ def get_mfrs(graph, source, target, verbose = False, mode = "es"):
                 id.append(oggraph.get_eid(chunk[0], chunk[1]))
                 id.sort()
             ids.append(id)
+
         if verbose:
             for id in ids:
                 print(ind, ":", id, "\n")
                 ind += 1
+
         return [ids, len(ids)]
